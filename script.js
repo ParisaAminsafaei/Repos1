4880fe7f39b2b05560418de89bf12be8c4aa3f38
Mohammad Aminsafaei <arc13.net@hotmail.com>
2022-12-22 12:14:14 +0330

    add html & css &js

---
+++ script.js
@@ -0,0 +1,35 @@
+'use strict'
+
+var phone_number = document.querySelector('#phone_number');
+
+phone_number.addEventListener('input' , function() {
+    if(phone_number.value.length < 11) {
+        phone_number.style.borderColor = 'red'
+        phone_number.style.color = 'red'
+    } else if(phone_number.value.length == '11') {
+        phone_number.style.borderColor = 'green'
+        phone_number.style.color = 'green'
+    }
+});
+
+//===========================================================================
+
+var button = document.querySelector('#button')
+
+button.addEventListener('click' , function() {
+    var value = document.querySelector('#phone_number');
+    var error3 = document.querySelector('.error3');
+    var error2 = document.querySelector('.error2');
+    if(value.value == '') {
+        error3.style.display = 'flex'
+    } else if(value.value == '09365953491') {
+        var error1 = document.querySelector('.error1')
+        error1.style.display = 'flex'
+        error2.style.display = 'none'
+        error3.style.display = 'none'
+    } else if(value.value != '09365953491') {
+        error2.style.display = 'flex'
+        error3.style.display = 'none'
+    }
+
+});
